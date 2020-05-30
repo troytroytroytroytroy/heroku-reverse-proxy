7a2c1f846a2d75ebd6901c2b962df2448804720f
Shantanu Khandelwal <shantanu561993@gmail.com>
2020-05-30 18:29:03 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ wget "https://github.com/jpillora/chisel/releases/download/v1.5.2/chisel_1.5.2_l
 mv chisel_1.5.2_linux_amd64.gz chisel.gz
 gunzip chisel.gz
 chmod +x chisel
-./chisel server  --reverse  --proxy "https://www.google.com" --auth "intruder:intruder"
+./chisel server  --reverse  --proxy "https://www.google.com" --auth "intruder:intruder" -p 8080 &
