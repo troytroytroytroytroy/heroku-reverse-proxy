e55b4ac8b0345d53a140c321206df5cf183a06e6
Shantanu Khandelwal <shantanu561993@gmail.com>
2020-06-09 00:11:52 +0800

    Delete install.sh

--- install.sh
+++
@@ -1,5 +0,0 @@
-wget "https://github.com/jpillora/chisel/releases/download/v1.5.2/chisel_1.5.2_linux_amd64.gz"
-mv chisel_1.5.2_linux_amd64.gz chisel.gz
-gunzip chisel.gz
-chmod +x chisel
-./chisel server  --reverse  --proxy "https://www.google.com" --auth "intruder:intruder" -p 8080 &
